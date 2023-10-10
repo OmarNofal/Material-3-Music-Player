fea4f8072c2c8791fffa5d09bc86f62c03db82c3
Omar Walid <omarwalidhamed@gmail.com>
2023-10-10 09:13:36 +0300

    Refactored MediaService and changed notification icons

--- core/playback/src/main/java/com/omar/musica/playback/PlaybackService.kt
+++ core/playback/src/main/java/com/omar/musica/playback/PlaybackService.kt
@@ -3,27 +3,23 @@ package com.omar.musica.playback
 import android.app.PendingIntent
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.media3.common.AudioAttributes
 import androidx.media3.common.C.AUDIO_CONTENT_TYPE_MUSIC
 import androidx.media3.common.C.USAGE_MEDIA
 import androidx.media3.common.Player
 import androidx.media3.exoplayer.ExoPlayer
-import androidx.media3.exoplayer.SeekParameters
 import androidx.media3.session.CommandButton
-import androidx.media3.session.DefaultMediaNotificationProvider
-import androidx.media3.session.MediaNotification
 import androidx.media3.session.MediaSession
 import androidx.media3.session.MediaSessionService
 import androidx.media3.session.SessionCommand
 import androidx.media3.session.SessionResult
-import com.google.common.collect.ImmutableList
 import com.google.common.util.concurrent.Futures
 import com.google.common.util.concurrent.ListenableFuture
+import timber.log.Timber
 
 class PlaybackService : MediaSessionService() {
 
-    private lateinit var player : Player
+    private lateinit var player: Player
     private lateinit var mediaSession: MediaSession
 
     @androidx.annotation.OptIn(androidx.media3.common.util.UnstableApi::class)
@@ -32,7 +28,8 @@ class PlaybackService : MediaSessionService() {
 
         player = ExoPlayer.Builder(applicationContext)
             .setAudioAttributes(
-                AudioAttributes.Builder().setContentType(AUDIO_CONTENT_TYPE_MUSIC).setUsage(USAGE_MEDIA).build(),
+                AudioAttributes.Builder().setContentType(AUDIO_CONTENT_TYPE_MUSIC)
+                    .setUsage(USAGE_MEDIA).build(),
                 true
             )
             .setHandleAudioBecomingNoisy(true)
@@ -40,8 +37,46 @@ class PlaybackService : MediaSessionService() {
 
 
 
+
+        mediaSession = MediaSession
+            .Builder(applicationContext, player)
+            .setCallback(buildCustomCallback())
+            .setCustomLayout(buildCommandButtons())
+            .setSessionActivity(buildPendingIntent())
+            .build()
+    }
+
+
+    private fun buildPendingIntent(): PendingIntent {
+        val intent = Intent(this, Class.forName("com.omar.musica.MainActivity")).apply {
+            action = VIEW_MEDIA_SCREEN_ACTION
+        }
+        return PendingIntent.getActivity(this, 1, intent, PendingIntent.FLAG_IMMUTABLE)
+    }
+
+
+    private fun buildCommandButtons(): List<CommandButton> {
+        val rewindCommandButton = CommandButton.Builder()
+            .setEnabled(true)
+            .setDisplayName("Jump Backward")
+            .setSessionCommand(SessionCommand(CUSTOM_COMMAND_JUMP_BACKWARD, Bundle()))
+            .setIconResId(R.drawable.outline_fast_rewind_24).build()
+        val fastForwardCommandButton = CommandButton.Builder()
+            .setEnabled(true)
+            .setSessionCommand(SessionCommand(CUSTOM_COMMAND_JUMP_FORWARD, Bundle()))
+            .setDisplayName("Jump Forward")
+            .setIconResId(R.drawable.outline_fast_forward_24).build()
+        return listOf(rewindCommandButton, fastForwardCommandButton)
+    }
+
+    override fun onGetSession(controllerInfo: MediaSession.ControllerInfo): MediaSession {
+        Timber.i(TAG, "Controller request: ${controllerInfo.packageName}")
+        return mediaSession
+    }
+
+    private fun buildCustomCallback(): MediaSession.Callback {
         val customCommands = buildCommandButtons()
-        val callback = object : MediaSession.Callback {
+        return object : MediaSession.Callback {
             override fun onConnect(
                 session: MediaSession,
                 controller: MediaSession.ControllerInfo
@@ -57,7 +92,6 @@ class PlaybackService : MediaSessionService() {
                     connectionResult.availablePlayerCommands
                 )
             }
-
 
 
             override fun onCustomCommand(
@@ -76,41 +110,6 @@ class PlaybackService : MediaSessionService() {
             }
         }
 
-        mediaSession = MediaSession
-            .Builder(applicationContext, player)
-            .setCallback(callback)
-            .setCustomLayout(buildCommandButtons())
-            .setSessionActivity(buildPendingIntent())
-            .build()
-    }
-
-
-    private fun buildPendingIntent(): PendingIntent {
-        val intent = Intent(this, Class.forName("com.omar.musica.MainActivity")).apply {
-            action = VIEW_MEDIA_SCREEN_ACTION
-        }
-        return PendingIntent.getActivity(this, 1, intent, PendingIntent.FLAG_IMMUTABLE)
-    }
-
-
-
-    private fun buildCommandButtons(): List<CommandButton> {
-        val rewindCommandButton = CommandButton.Builder()
-            .setEnabled(true)
-            .setDisplayName("Jump Backward")
-            .setSessionCommand(SessionCommand(CUSTOM_COMMAND_JUMP_BACKWARD, Bundle()))
-            .setIconResId(R.drawable.baseline_fast_rewind_24).build()
-        val fastForwardCommandButton = CommandButton.Builder()
-            .setEnabled(true)
-            .setSessionCommand(SessionCommand(CUSTOM_COMMAND_JUMP_FORWARD, Bundle()))
-            .setDisplayName("Jump Forward")
-            .setIconResId(R.drawable.baseline_fast_forward_24).build()
-        return listOf(rewindCommandButton, fastForwardCommandButton)
-    }
-
-    override fun onGetSession(controllerInfo: MediaSession.ControllerInfo): MediaSession? {
-        Log.i(TAG, "Controller request: ${controllerInfo.packageName}")
-        return mediaSession
     }
 
     override fun onDestroy() {
@@ -123,8 +122,6 @@ class PlaybackService : MediaSessionService() {
 
     companion object {
         const val TAG = "MEDIA_SESSION"
-        const val ACTION_PLAY = "PLAY"
-        const val EXTRA_URI = "EXTRA_URI"
         const val VIEW_MEDIA_SCREEN_ACTION = "MEDIA_SCREEN_ACTION"
         const val CUSTOM_COMMAND_JUMP_FORWARD = "JUMP_FORWARD"
         const val CUSTOM_COMMAND_JUMP_BACKWARD = "JUMP_BACKWARD"
